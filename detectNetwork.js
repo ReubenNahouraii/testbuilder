14904d9c3d9b0c506162debf64c22a92a90190a8
GrokN <FullGrok@gmail.com>
2017-04-03 22:27:01 -0700

    Add additional credit card support
    Added support for additional credit cards, ie, MasterCard and Visa.
    Now credits cards supported are Diner's Club, American Express,
    MasterCard, and Visa

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,38 +1,48 @@
-// Given a credit card number, this function should return a string with the
-// name of a network, like 'MasterCard' or 'American Express'
-// Example: detectNetwork('343456789012345') should return 'American Express'
-
-// How can you tell one card network from another? Easy!
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
+// Reuben Nahouraii
+// 4-3-17
 
 // credit card object to hold all the credit card specifications
 let creditcards =
 {
   americanExpress : {
     name : "American Express",
-    firstTwo : [34, 37],
-    length : 15
+    startingDigits : [34, 37],
+    length : [15]
   },
   dinersClub : {
     name : "Diner's Club",
-    firstTwo : [38, 39],
-    length : 14
+    startingDigits : [38, 39],
+    length : [14]
+  },
+  visa : {
+    name : "Visa",
+    startingDigits : [4],
+    length : [13, 16, 19]
+  },
+  mastercard : {
+    name : "MasterCard",
+    startingDigits : [51, 52, 53, 54, 55],
+    length : [16]
   }
 }
 
 // checks the first two numbers of a given card to determine the network
-function checkFirstTwo(cardNumber, firstTwoArray) {
+function checkStartingDigits(cardNumber, startingDigitsArray) {
   // debugger
-  for(let i = 0; i < firstTwoArray.length; i++)
-    if(cardNumber.substring(0,2) == firstTwoArray[i])
+  let endIndex = String(startingDigitsArray[0]).length;
+  for(let i = 0; i < startingDigitsArray.length; i++)
+    if(cardNumber.substring(0, endIndex) == startingDigitsArray[i])
       return true
   return false
 }
 
 // checks the length of a given card to determine the network
-function checkLength(cardNumber, length) { return cardNumber.length === length }
+function checkLength(cardNumber, lengthArray) {
+  for(let i = 0; i < lengthArray.length; i++)
+    if(cardNumber.length === lengthArray[i])
+      return true
+  return false
+}
 
 // searches for a credit card given a credit card number and credit card object
 function findCard(cardNumber) {
@@ -41,7 +51,7 @@ function findCard(cardNumber) {
     let cardObj = this[card]
     if
     (
-      checkFirstTwo(cardNumber, cardObj.firstTwo) &&
+      checkStartingDigits(cardNumber, cardObj.startingDigits) &&
       checkLength(cardNumber, cardObj.length)
     )
       return cardObj.name
@@ -49,12 +59,26 @@ function findCard(cardNumber) {
   return 'Card Not Found'
 }
 
+// Given a credit card number, this function should return a string with the
+// name of a network, like 'MasterCard' or 'American Express'
 var detectNetwork = function(cardNumber) {
   return findCard.call(creditcards, cardNumber)
 };
 
-console.log('running tests')
-console.log('should be diner club === ', detectNetwork('38345678901234'))
-console.log('should be diner club === ', detectNetwork('39345678901234'))
-console.log('should be American Express === ', detectNetwork('343456789012345'))
-console.log('should be American Express === ', detectNetwork('373456789012345'))
+// test cases
+function testNetworks()
+{
+  detectNetwork('38345678901234')
+  detectNetwork('39345678901234')
+  detectNetwork('343456789012345')
+  detectNetwork('373456789012345')
+
+  detectNetwork('4123456789012')
+  detectNetwork('4123456789012345')
+  detectNetwork('4123456789012345678')
+  detectNetwork('5112345678901234')
+  detectNetwork('5212345678901234')
+  detectNetwork('5312345678901234')
+  detectNetwork('5412345678901234')
+  detectNetwork('5512345678901234')
+}
